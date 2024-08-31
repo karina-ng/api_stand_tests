f4fc5db7491cd5151b785fad1f70cfe7db53fe60
karina-ng <karina.ng275@gmail.com>
2024-08-30 22:31:31 -0700

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-3438edc6-32ab-45bd-9dfb-1a44ceff2529.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,21 @@
+import configuration
+import requests
+import data
+from data import product_ids
+
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+def post_products_kits(products_ids):
+    # Realiza una solicitud POST para buscar kits por productos.
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, # Concatenación de URL base y ruta.
+                         json=products_ids, # Datos a enviar en la solicitud.
+                         headers=data.headers) # Encabezados de solicitud.
+
+
